b3e9a838a4b1cf69e5c11e5f47c925fd6d43d607
Tomasz Płocic <tplocic20@gmail.com>
2024-09-02 16:54:08 +0200

    DataCap Flow Sankey and Tree use the same data now (#17)

--- src/components/DashboardPageV2/dataCapFlowGraph.js
+++ src/components/DashboardPageV2/dataCapFlowGraph.js
@@ -1,177 +1,137 @@
-import { useFetch } from '../../hooks/fetch';
 import { convertBytesToIEC } from '../../utils/bytes';
-import { useEffect, useMemo, useState } from 'react';
+import { useMemo, useState } from 'react';
 import { ResponsiveContainer, Sankey, Tooltip } from 'recharts';
-import filesize from 'filesize';
 import s from './s.module.css';
-import { xFormatter } from '../../utils/chart';
-import { ArrowLeft, ArrowRight } from 'lucide-react';
-import { Link } from 'react-router-dom';
 import cn from 'classnames';
+import useDataCapFlow from '../../hooks/useDataCapFlow';
 
-const PB_10 = 10 * 1024 * 1024 * 1024 * 1024 * 1024;
-const PB_15 = 15 * 1024 * 1024 * 1024 * 1024 * 1024;
 
 export const DataCapFlowGraph = () => {
-  const fetchUrl = '/get-dc-flow-graph';
+  const {
+    dataCapFlow
+  } = useDataCapFlow(false);
 
-  const [data, { loading, loaded }] = useFetch(fetchUrl);
-  const [selectedNodes, setSelectedNodes] = useState(['rkh']);
+  const [selectedNodes, setSelectedNodes] = useState([{
+    id: 0,
+    name: 'Root Key Holder',
+  }]);
 
   const renderTooltip = (props) => {
     const linkData = props?.payload?.[0]?.payload?.payload;
+    const nodeData = linkData?.target ?? linkData;
+    if (!nodeData) {
+      return <></>
+    }
+    const {
+      name,
+      datacap,
+      allocators,
+      hasChildren,
+      children
+    } = nodeData
     return (<>
-      {linkData?.target && <div key={0} className={s.chartTooltip}>
+      <div key={1} className={s.chartTooltip}>
         <div className={s.chartTooltipTitle}>
-          {linkData?.target?.name}
+          {name}
         </div>
         <div className={s.chartTooltipData}>
-          {linkData?.target?.value} {linkData?.unit || ''}
+          {convertBytesToIEC(datacap)} Datacap
         </div>
-      </div>}
-      {linkData?.name && <div key={1} className={s.chartTooltip}>
-        <div className={s.chartTooltipTitle}>
-          {linkData?.name === 'rkh' ? 'Root Key Holder' : linkData?.name}
-        </div>
-        <div className={s.chartTooltipData}>
-          {convertBytesToIEC(linkData?.subtext)} Datacap
-        </div>
-      </div>}
+        {allocators > 0 && <div className={s.chartTooltipData}>
+          {allocators} Allocators
+        </div>}
+      </div>
     </>);
   };
 
   const handleNodeClick = (data) => {
-    const name = data?.payload?.name || data?.payload?.target?.name;
-    if (selectedNodes.length === 3 || selectedNodes[selectedNodes.length - 1] === name)  {
+    if (data.payload?.target) {
       return;
     }
-    setSelectedNodes([...selectedNodes, name]);
-  };
+    const hasChildren = data?.payload?.hasChildren;
+    const isParent = data?.payload?.isParent;
+    const name = data?.payload?.name;
+    const lastSelectedNode = selectedNodes[selectedNodes.length - 1];
 
-  const handleBackClick = () => {
-    setSelectedNodes(selectedNodes.slice(0, selectedNodes.length - 1));
-  };
-
-  const sankeyLevel2 = (allocators, parent) => {
-
-    const totalAvailableDatacap = Object.values(allocators).reduce((acc, item) => acc + +item.datacap, 0);
-    const amountOfClients = Object.values(allocators).reduce((acc, item) => acc + Object.values(item.clients).length, 0);
-    const dataCapUsedByClients = Object.values(allocators).map(item => Object.values(item.clients)).flat(1).reduce((acc, item) => acc + +item.datacap, 0)
-
-    let skData = {
-      [parent]: {
-        nodes: [{
-          name: parent,
-          subtext: Object.values(allocators).reduce((acc, item) => acc + +item.datacap, 0)
-        }, {
-          name: `${amountOfClients} Clients`,
-          subtext: dataCapUsedByClients
-        }, {
-          name: 'Unassigned',
-          subtext: totalAvailableDatacap - dataCapUsedByClients
-        }],
-        links: [{
-          source: 0,
-          target: 1,
-          value: Math.round(dataCapUsedByClients / totalAvailableDatacap * 100),
-          unit: '%'
-        },{
-          source: 0,
-          target: 2,
-          value: Math.round(100 - (dataCapUsedByClients / totalAvailableDatacap * 100)),
-          unit: '%'
-        }]
-      }
+    if (lastSelectedNode.name === name && isParent) {
+      setSelectedNodes(selectedNodes.slice(0, - 1));
+      return
     }
 
-    return skData;
-  }
-
-  const sankeyLevel1 = (allocators, parent) => {
-
-    let skData = {
-      [parent]: {
-        nodes: [{
-          name: parent,
-          subtext: Object.values(allocators).reduce((acc, item) => acc + +item.datacap, 0)
-        }],
-        links: []
-      }
+    if (!hasChildren) {
+      return
     }
 
-    Object.entries(Object.groupBy(allocators, item => convertBytesToIEC(item.datacap))).forEach(([dc, allocators], index) => {
-      skData[parent].nodes.push({
-        name: dc,
-        subtext: Object.values(allocators).reduce((acc, item) => acc + +item.datacap, 0)
-      });
-      skData[parent].links.push({
-        source: 0,
-        target: index + 1,
-        value: allocators.length,
-        unit: 'Allocators'
-      });
-      skData = {...skData, ...sankeyLevel2(allocators, dc)};
-    })
-    return skData;
-  }
+    const arrayIndex = data?.payload?.arrayIndex;
+
+    setSelectedNodes([...selectedNodes, {
+      id: arrayIndex,
+      name: name,
+    }]);
+  };
+
+  const handleBackClick = (backIndex) => {
+    setSelectedNodes(selectedNodes.slice(0, backIndex+1));
+  };
 
   const sankeyData = useMemo(() => {
-    if (!data?.rkh) {
+    if (!dataCapFlow?.length) {
       return null;
     }
 
-    let skData = {
-      'rkh': {
-        nodes: [{
-          name: 'Root Key Holder',
-          subtext: Object.values(data?.rkh)?.reduce((acc, item) => acc + +item.datacap, 0)
-        }],
-        links: []
-      }
-    };
+    let currentNode = dataCapFlow[selectedNodes[0].id];
+    for (let i = 1; i < selectedNodes.length; i++) {
+      currentNode = currentNode.children[selectedNodes[i].id];
+    }
 
-    const datacapAllocatorsGrouped = Object.groupBy(Object.values(data.rkh), item => {
-      if (item.datacap < PB_10) {
-        return '<10 PiB';
-      } else if (item.datacap < PB_15) {
-        return '>10 PiB & <15 PiB';
-      } else {
-        return '>15 PiB';
-      }
-    });
-
-    Object.entries(datacapAllocatorsGrouped).forEach(([datacap, allocators], index) => {
-      skData['rkh'].nodes.push({
-        name: datacap,
-        subtext: Object.values(allocators).reduce((acc, item) => acc + +item.datacap, 0)
-      });
-      skData['rkh'].links.push({
+    return {
+      nodes: [{
+        name: currentNode?.name,
+        datacap: currentNode?.attributes?.datacap,
+        allocators: currentNode?.attributes?.allocators,
+        isParent: selectedNodes.length > 1,
+      }, ...currentNode?.children?.map((child, index) => ({
+        name: child?.name,
+        datacap: child?.attributes?.datacap,
+        allocators: child?.attributes?.allocators,
+        arrayIndex: index,
+        children: child?.children,
+        hasChildren: !!child?.children?.map(item => !!item.children?.length).filter(val => !!val).length
+      })) || []],
+      links: currentNode?.children?.map((child, index) => ({
         source: 0,
         target: index + 1,
-        value: allocators.length,
-        unit: 'Allocators'
-      });
+        arrayIndex: index,
+        value: child?.attributes?.datacap,
+        datacap: child?.attributes?.datacap,
+        allocators: child?.attributes?.allocators,
+        children: child?.children,
+        hasChildren: !!child?.children?.map(item => !!item.children?.length).filter(val => !!val).length
+      })) || []
+    }
 
-      skData = {...skData, ...sankeyLevel1(allocators, datacap)};
-    });
+  }, [dataCapFlow, selectedNodes]);
 
-
-    return skData;
-
-  }, [data]);
-
-  return <>
-    {selectedNodes.length > 1 && <div className={s.selectedNodes}>
-      <button className={cn(s.cardLink, s.backLink)} onClick={handleBackClick}>
-        <ArrowLeft size={18} />
-        {selectedNodes[selectedNodes.length - 2] === 'rkh' ? 'Root Key Holder' : selectedNodes[selectedNodes.length - 2]}
-      </button>
-    </div>}
+  return <div className="relative">
+    <div className={s.selectedNodes}>
+      {
+        selectedNodes.map((node, index) => {
+          return <>
+          <button className={cn(s.cardLink, s.backLink, index === selectedNodes.length - 1 && s.inactive)} onClick={() => handleBackClick(index)}>
+            {node.name}
+          </button>
+          {
+            index < selectedNodes.length - 1 && <div className={s.separator} />
+          }
+          </>;
+        })
+      }
+    </div>
     {sankeyData && <ResponsiveContainer width="100%" aspect={2} debounce={100}>
       <Sankey
         width={960}
         height={500}
-        data={sankeyData[selectedNodes[selectedNodes.length - 1]]}
+        data={sankeyData}
         nodePadding={50}
         node={<Node />}
         margin={{
@@ -181,18 +141,26 @@ export const DataCapFlowGraph = () => {
           bottom: 100
         }}
         onClick={handleNodeClick}
-        link={{ stroke: 'var(--color-medium-turquoise)', cursor: 'pointer' }}
+        link={{ stroke: 'var(--color-medium-turquoise)'}}
       >
         <Tooltip content={renderTooltip} />
       </Sankey>
     </ResponsiveContainer>}
-  </>;
+  </div>;
 };
 
 const Node = ({ x, y, width, height, index, payload }) => {
+  const hasChildren = payload.hasChildren || payload.isParent;
   return <g transform={`translate(${x},${y})`} style={{ cursor: 'pointer' }}>
-    <rect x={-width / 2} y={0} width={width * 1.5} height={height} fill="var(--color-dodger-blue)" />
-    <text x={width * 1.5} y={height / 2} fill="black" fontSize={12}>
+    <rect x={-width}
+          y={-5}
+          width={width * 2}
+          rx={4}
+          height={height + 10}
+          cursor={hasChildren ? 'pointer' : 'default'}
+          fill={hasChildren ? 'var(--color-dodger-blue)' : 'var(--color-horizon)'}/>
+    <text x={width * 1.5} y={height / 2} fill="black" fontSize={12}
+          cursor={hasChildren ? 'pointer' : 'default'}>
       {payload.name}
     </text>
   </g>;
